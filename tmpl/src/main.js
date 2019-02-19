1d6e7c907809d054a5195dddb44dfb7f597a9f90
chenweitech <chenweitech@outlook.com>
2019-02-19 16:54:28 +0800

    feat: 更新模版文件

--- tmpl/src/main.js
+++ tmpl/src/main.js
@@ -9,14 +9,32 @@ import App from './App.vue';
 import API from './utils/api';
 import * as ReturnCode from './utils/returnCode';
 
+import authorizationPlugin from './authorizationPlugin';
 import './assets/css/index.css';
+
+import GlobalConfig from './configs/GlobalConfig';
+
+// 如果使用mock数据，则引入下面的文件，如不引用，请注释
+if(process.env.MOCK){
+  const Mock = require('./mock/index.js');
+} 
 
 Vue.use(ElementUI);
 
 Vue.prototype.$http = API.api;
 Vue.prototype._returnCode = ReturnCode;
+Vue.prototype.$_has = function (desc) {
+  if(GlobalConfig.withGlobalAuthotrization) {
+    return this.$root.globalResourcesPermissionsArray.some(item => {
+      return item == desc
+    })
+  }else{
+    return true
+  }
+}
 
 new Vue({
+  mixins: [authorizationPlugin],    // 权限注入插件
   router,
   // {{#vuex}}
   store,
